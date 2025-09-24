df21915a266c6c4aef4e040a92e9fbd630e3c807
Paulo Victor <paulo.andrade@planejamento.rj.gov.br>
2025-09-24 08:40:57 -0300

    H2 database, test profile and JPA

---
+++ workshop/src/main/java/com/example/workshop/resources/UserResource.java
@@ -0,0 +1,15 @@
+package com.example.workshop.resources;
+
+import com.example.workshop.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+//    public ResponseEntity<User> findAll() {
+//
+//    }
+}
