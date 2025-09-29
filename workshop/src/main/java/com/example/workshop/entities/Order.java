09645fce2a36ff389f79ba6fbbecf20654bcd5af
Paulo Victor <paulo.andrade@planejamento.rj.gov.br>
2025-09-29 09:52:40 -0300

    Total and SubTotal methods

--- workshop/src/main/java/com/example/workshop/entities/Order.java
+++ workshop/src/main/java/com/example/workshop/entities/Order.java
@@ -90,6 +90,14 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public Double getTotal(){
+        double total = 0;
+        for (OrderItem item : items) {
+            total += item.getPrice();
+        }
+        return total;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
